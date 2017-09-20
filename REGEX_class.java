5826782b7b957af2b710e9880a2967e928329a64
Zac Gallagher <zgallagher@luc.edu>
2017-09-20 18:58:38 +0000

    Regex

--- REGEX_class.java
+++ REGEX_class.java
@@ -1,4 +1,14 @@
 //REGEX file
+ //asks user for what they want
+  /*
+  ! = UNIQUE identifier for note
+^ = denotes a reference to another note by their unique identifier (above)
+@ = mentions a PERSON, unique to the individual person
+# = denotes a topic addressed in the note
+[keywords] = "vocab words," or common words within a note
+  */
+  []
+
 public class REGEX_class {  // class declaration
     
     
@@ -18,12 +28,46 @@ public class REGEX_class {  // class declaration
     
     
     
-    public void mentions {
+    public void mentions { //@mentions function
         
         
         
         
     }
+    
+       public void keywords { //[keywords] function
+        
+        
+        
+        
+    }
+    
+    
+       public void topics { //#topics function
+        
+        
+        
+        
+    }
+    
+      public void topics { //^reference function
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+      public void topics { //!unique identifier function
+        
+        
+        
+        
+    }
+    
+    
     
     
     
