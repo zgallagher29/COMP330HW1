7883068fc2cd98dac498e1e07f4324f50e2c1dcc
Zac Gallagher <zgallagher@luc.edu>
2017-09-20 18:52:46 +0000

    Working on REGEX_class commit

--- REGEX_class.java
+++ REGEX_class.java
@@ -1,3 +1,38 @@
 //REGEX file
-
-
+public class REGEX_class {  // class declaration
+    
+    
+    //we need to have a constructor
+    
+    public REGEX_class { //constructor
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    //methods:
+    
+    
+    
+    public void mentions {
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
