1feb2980cea217e492f1bcf0a90730b0589f6489
Zac Gallagher <zgallagher@luc.edu>
2017-09-22 02:57:25 +0000

    This is still  hard

--- Note.java
+++ Note.java
@@ -17,7 +17,7 @@ public class Note {
 
  List<String> fileName= new ArrayList<>();
  
- rTest test = new rTest();
+ 
 
 public void start(){
     String userFolderName="";
@@ -43,20 +43,110 @@ public void start(){
     System.out.println("No files here!");
     
   }
-  test.startRegex();
   
   
+  
+int mention_option = 0;
+keyboard = new Scanner(System.in);
+do{
+System.out.println("Are you looking for a 1. A Unique Identifier (!) 2. A Reference (^) 3. A Person (@) 4. A Topic (#) 5. Keyword ");
+mention_option = keyboard.nextInt();
+}while ((mention_option>5) || (mention_option <1));  //need to catch if it's not an integer
+
+
+String searchWord= null;
+String addedCharacter;
+
+searchWord=keyboard.nextLine();//idk why this has to be there but it skips the switch statement if it's not there
+switch (mention_option) {
+            
+            case 1:  
+            
+            System.out.print("1. What unique indentifier would you look like to search for? (!,*,?,_etc.) ");
+            addedCharacter=keyboard.nextLine();
+             System.out.print("2. What keyword would you look like to search for? ");
+            searchWord=keyboard.nextLine();
+            searchWord=searchWord+addedCharacter;
+            break;
+            
+            case 2: 
+            addedCharacter = "^";
+            System.out.print("2. What reference would you look like to search for? ");
+            searchWord=keyboard.nextLine();
+            searchWord=searchWord+addedCharacter;
+            break;
+            
+            case 3:  
+            addedCharacter = "@";
+            System.out.print("3. Who would you look like to search for? ");
+            searchWord=keyboard.nextLine();
+            searchWord=addedCharacter+searchWord;
+            break;
+            
+            case 4:  
+            addedCharacter = "#";
+            System.out.print("4. What topic would you look like to search for? ");
+            searchWord=keyboard.nextLine();
+            searchWord=addedCharacter+searchWord;
+            break;
+            
+            case 5: 
+             System.out.print("2. What keyword would you look like to search for? ");
+             searchWord=keyboard.nextLine();
+             break;
+                                        }
+                            
+String line="";
+int totalMentions=0;
+int lineNumber=1;
+i=0;
+ for (File child : directoryListing){
     
-    
-    
+      try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = 
+                new FileReader(directoryListing[i]);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+              
+               
+                if(line.contains(searchWord)){
+               System.out.println("In line number "+ lineNumber+ ": ");
+                System.out.println(line);
+                totalMentions++;
+                    
+                }
+                lineNumber++;
+               
+               
+            }
+            System.out.println("There were " +totalMentions+ " occurances of " + searchWord +" in " +directoryListing[i]);
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                directoryListing[i] + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + directoryListing[i] + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+    i++;
+    totalMentions=0;
+    lineNumber=1;
 }
-
-
-
-
-
 }
-
+}
 
  //ok so we need a function involving REGEX to parse through the files
  //Then we add these phrases + the file name to a hashmap
--- REGEX_class.java
+++ REGEX_class.java
@@ -2,7 +2,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.*;
 
-public class rTest {
+public class regTest {
  
  
  
@@ -12,8 +12,10 @@ public class rTest {
  
   public void startRegex() {
     String in = "Let's match 127.0.0.1 being valid, or this IP: 127.0.0.1 and joinTHIS.server.com or build 1.2";
-    String re = "\\b((?:(?:[0-9]{1,3}\\.){3}[0-9]{1,3}|(?:[a-z0-9]+(?:-[a-z0-9]+)*\\.)+[a-z]{2,4}))\\b";
-    Pattern p = Pattern.compile(re, Pattern.CASE_INSENSITIVE);
+    String regularexpressioncheck = "\\b((?:(?:[0-9]{1,3}\\.){3}[0-9]{1,3}|(?:[a-z0-9]+(?:-[a-z0-9]+)*\\.)+[a-z]{2,4}))\\b"; //since we're doing it all to search
+    //in one string we need to search through and say like '+' so that it looks for multiple
+    
+    Pattern p = Pattern.compile(regularexpressioncheck, Pattern.CASE_INSENSITIVE);
     Matcher m = p.matcher(in);
     while (m.find()) {
       System.out.println(m.group(1));
